3d6e7896a560fb6c880edd07bebefe76cc5593ec
masha <catboymasha@gmail.com>
2022-09-16 17:43:11 +0200

    new features

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 const prompt = require('prompt-sync')();
 const alpha = ` abcdefghijklmnopqrstuvwxyz0123456789,;.:-_'*?+!@£#¤$%&/{([)]=}<>""''`;
+
 const scripts = {
+    enc: function () {
+        var msg = prompt('message to encrypt > ');
+        var msg_num = [];
+        for (let i = 0; i < msg.length; i++) {
+            msg_num = [...msg_num, alpha.indexOf(msg[i])];
+        }
+        var inpkey = prompt("key (leave empty to generate) > ");
+        if (inpkey.length > 0) {
+            var otp = [];
+            for (let i = 0; i < inpkey.split(" ").length; i++) {
+                otp = [...otp, parseInt(inpkey.split(" ")[i], 16)];
+            }
+        } else {
+            var otp = [];
+            for (let i = 0; i < msg.length; i++) {
+                otp = [...otp, parseInt(Math.floor(Math.random() * 128) + 1, 10)];
+            }
+        }
+        var encrypted_message_num = [];
+        for (let i = 0; i < msg_num.length; i++) {
+            encrypted_message_num = [...encrypted_message_num,
+                parseInt(msg_num[i], 10) + parseInt(otp[i], 10)]
+        }
+        var otp_hex = [];
+        for (let i = 0; i < otp.length; i++) {
+            otp_hex = [...otp_hex, parseInt(otp[i], 10).toString(16)];
+        }
+        var encrypted_message_hex = [];
+        for (let i = 0; i < encrypted_message_num.length; i++) {
+            encrypted_message_hex = [...encrypted_message_hex, 
+                parseInt(encrypted_message_num[i], 10).toString(16)];
+        }
+        var otp_string = otp_hex.toString().replace(/,/g, " ");
+        var encrypted_message_string = encrypted_message_hex
+            .toString().replace(/,/g, " ");
+        return "msg="+encrypted_message_string+"\nkey="+otp_string
+                +"\nraw="+encrypted_message_string+" // "+otp_string;
+    },
     dec: function () {
-        var msg = prompt("message to decrypt > ").split(" ");
-        var key = prompt("key > ").split(" ");
+        var rawinp = prompt("message to decrypt > ")
+        if (rawinp.includes("//")) {
+            var msg = rawinp.split(" // ")[0].split(" ")
+            var key = rawinp.split(" // ")[1].split(" ")
+        } else {
+            var msg = rawinp.split(" ");
+            var key = prompt("decryption key > ").split(" ");
+        }   
         var msg_num = [];
         for (let i = 0; i < msg.length; i++) {
             msg_num = [...msg_num, parseInt(msg[i], 16)];   
@@ -25,45 +70,13 @@ const scripts = {
         decrypted_message_string = decrypted_message_string
             .toString().replace(/,/g, " ").replace(/(.{1})./g,"$1");
         return decrypted_message_string;
-    },
-    enc: function () {
-        var msg = prompt('message to encrypt > ');
-        var msg_num = [];
-        for (let i = 0; i < msg.length; i++) {
-            msg_num = [...msg_num, alpha.indexOf(msg[i])];
-        }
-        var otp = [];
-        for (let i = 0; i < msg.length; i++) {
-            otp = [...otp, parseInt(Math.floor(Math.random() * 128) + 1, 10)];
-        }
-        var encrypted_message_num = [];
-        for (let i = 0; i < msg_num.length; i++) {
-            encrypted_message_num = [...encrypted_message_num,
-                parseInt(msg_num[i], 10) + parseInt(otp[i], 10)]
-        }
-        var encrypted_message_hex = [];
-        for (let i = 0; i < encrypted_message_num.length; i++) {
-            encrypted_message_hex = [...encrypted_message_hex, 
-                parseInt(encrypted_message_num[i], 10).toString(16)];
-        }
-        var otp_hex = [];
-        for (let i = 0; i < otp.length; i++) {
-            otp_hex = [...otp_hex, parseInt(otp[i], 10).toString(16)];
-        }
-        var otp_string = otp_hex.toString().replace(/,/g, " ");
-        var encrypted_message_string = encrypted_message_hex
-            .toString().replace(/,/g, " ");
-        return "msg = "+encrypted_message_string+"\nkey = "+otp_string;
-
     }
 }
-switch (prompt("encrypt or decrypt? (e/d) > ")) {
+
+switch(prompt("Encrypt or Decrypt (e/d) > ")) {
     case "e":
         console.log(scripts.enc());
         break;
     case "d":
         console.log(scripts.dec());
-        break;
 }
-
-
